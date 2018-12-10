df4468f6301a1f38a3ce45f5f0214db0fe3157d4
Michał Chyb <michalchyb1@gmail.com>
2018-12-10 20:31:36 +0100

    initial commit. Migration from Intellij to eclipse IDE

---
+++ src/main/java/pl/mchyb/mypage/MyPagev2Application.java
@@ -0,0 +1,13 @@
+package pl.mchyb.mypage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPagev2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPagev2Application.class, args);
+		System.out.println("test");
+	}
+}
