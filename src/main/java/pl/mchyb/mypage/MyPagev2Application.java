d833aa0676d6cfb67b41dd01202e531388c3633e
Michał Chyb <michalchyb1@gmail.com>
2018-12-19 20:27:53 +0100

    convert data from api to jsonObject,save it in file, refactorization

--- src/main/java/pl/mchyb/mypage/MyPagev2Application.java
+++ src/main/java/pl/mchyb/mypage/MyPagev2Application.java
@@ -1,16 +1,36 @@
 package pl.mchyb.mypage;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import pl.mchyb.mypage.geoip.JsonWeatherApiSingletone;
 
 @SpringBootApplication
 public class MyPagev2Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+			throws JsonParseException, JsonMappingException, IOException, ParseException {
 		SpringApplication.run(MyPagev2Application.class, args);
 
-		 System.out.println(JsonWeatherApiSingletone.getInstance().getDataFromApi());
+		String dataFromApi = JsonWeatherApiSingletone.getInstance().getDataFromApi();
+
+		JSONParser parser = new JSONParser();
+		JSONObject json = (JSONObject) parser.parse(dataFromApi);
+
+		String s = json.toJSONString();
+		BufferedWriter writer = new BufferedWriter(
+				new FileWriter("/home/michalch/eclipse-workspace/MyPagev2/src/main/resources/json/weather.json"));
+		writer.write(s);
+		writer.close();
 	}
 }
