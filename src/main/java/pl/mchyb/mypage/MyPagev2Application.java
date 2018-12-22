dcfe91dff66e271f0c52aaf9029df46d61c88088
Michał Chyb <michalchyb1@gmail.com>
2018-12-22 16:42:04 +0100

    testing for multiple localization, refactorization

--- src/main/java/pl/mchyb/mypage/MyPagev2Application.java
+++ src/main/java/pl/mchyb/mypage/MyPagev2Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
+import pl.mchyb.mypage.geoip.GeoIPv4;
 import pl.mchyb.mypage.helpers.Helpers;
 
 @SpringBootApplication
@@ -19,8 +20,21 @@ public class MyPagev2Application {
 			throws JsonParseException, JsonMappingException, IOException, ParseException {
 		SpringApplication.run(MyPagev2Application.class, args);
 
-		JSONObject jsonObject = Helpers.convertApiStringToJsonObject();
-		Helpers.saveToFile(jsonObject);
-	}
+		JSONObject jsonObject = null;
+		try {
+			System.out.println(GeoIPv4.getLocation(Helpers.getMyIp()).getCity());
+			jsonObject = Helpers.convertApiStringToJsonObject();
+			System.out.println(jsonObject.toJSONString());
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			Helpers.saveToFile(jsonObject);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
+	}
 }
--- src/main/java/pl/mchyb/mypage/controller/HomeController.java
+++ src/main/java/pl/mchyb/mypage/controller/HomeController.java
@@ -1,18 +1,13 @@
 package pl.mchyb.mypage.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import pl.mchyb.mypage.geoip.GeoIPv4;
-import pl.mchyb.mypage.helpers.Helpers;
-
 
 @Controller
 public class HomeController {
-    @RequestMapping("/index")
-    public String home() {
-        System.out.println(GeoIPv4.getLocation(Helpers.getMyIp()));
-        return "index";
-    }
+
+	@RequestMapping("/index")
+	public String home() {
+		return "index";
+	}
 }
